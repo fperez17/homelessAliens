7f250b7fdbf7d194bb788d6f500e790df0663914
fperez17 <117844972+fperez17@users.noreply.github.com>
2022-11-16 15:30:11 -0500

    Add files via upload

---
+++ Code/Choose-Character/choose.js
@@ -0,0 +1,8 @@
+function selectImage {
+
+}
+
+//in one way you want to store a (string) variable
+
+//tell your computer without having anything to store, i selectrd this image
+// second page: thats the pne got it
